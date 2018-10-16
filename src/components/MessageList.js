8275d707beb12f23a7b155dfef1d7f44692a9df7
Dre Recinos <drerecinos.web@gmail.com>
2018-10-16 16:38:29 -0700

    10.13 333

--- src/components/MessageList.js
+++ src/components/MessageList.js
@@ -1,80 +1,61 @@
 import React, { Component } from 'react';
-
-export class MessageList extends Component {
+ export class MessageList extends Component {
   constructor(props) {
     super(props);
-      this.state = { username: "", content: "", sentAt: "", roomId: "", messages: []}
-      this.messagesRef = this.props.firebase.database().ref("messages");
-      this.handleChange = this.handleChange.bind(this);
-      this.createMessage = this.createMessage.bind(this);
-      this.componentDidMount = this.componentDidMount.bind(this);
-  }
+      this.state = {
+        username: "<USERNAME HERE>",
+        content: "<CONTENT OF THE MESSAGE HERE>",
+        sentAt: "<TIME MESSAGE WAS SENT HERE>",
+        roomId: "<ROOM UID HERE>"
+      }
+     this.messageRef = this.props.firebase.database().ref('message');
+    this.handleChange = this.handleChange.bind(this);
+    this.createMessage = this.createRoom.bind(this);
+  }}
 
-  handleChange(e) {
-    e.preventDefault();
-    this.setState({
-      username: "user",
-      content: e.target.value,
-      sentAt: this.props.firebase.database.ServerValue.TIMESTAMP,
-      roomId: this.props.activeRoom
-    });
+  handleChange(e){
+    this.setState({ title: e.target.value })
+}
+   createMessage(e) {
+      e.preventDefault();
+      let NewMessage = this.state.content;
+      this.messageRef.push({ title: this.state.title });
+       this.setState({ title: "" });
   }
-
-  createMessage(e) {
-    e.preventDefault();
-    this.messagesRef.push({
-      username: this.state.username,
-      content: this.state.content,
-      sentAt: this.state.sentAt,
-      roomId: this.state.roomId
-    });
-    this.setState({ username: "", content: "", sentAt: "", roomId: "" });
-  }
-
-  componentDidMount() {
-    this.messagesRef.on('child_added', snapshot => {
+   componentDidMount() {
+      this.messageRef.on('child_added', snapshot => {
       const message = snapshot.val();
       message.key = snapshot.key;
-      this.setState({ messages: this.state.messages.concat(message) })
+      this.setState({ rooms: this.state.messages.concat( message ) })
     });
   }
-
-  render() {
-    const activeRoom = this.props.activeRoom;
-
-    const messageBar = (
-      <form onSubmit={ (e) => this.handleSubmit(e) }>
-      <input type="text" value={ this.state.newTodoDescription } onChange={ (e) => this.handleChange(e) } />
-    );
-
-
-    /*<form onSubmit={ (e) => this.handleSubmit(e) }>
-      <input type="text" value={ this.state.newTodoDescription } onChange={ (e) => this.handleChange(e) } /> */
-
-    const messageList = (
-      this.state.messages.map((message) => {
-        if (message.roomId === activeRoom) {
-          return <li key={message.key}>{message.content}</li>
-
+   render() {
+     const activeRoom = this.props.activeRoom;
+     const messageForm = (
       <form onSubmit={this.createMessage}>
-        <input type="text" value={this.state.content} placeholder="Enter Message" onChange={this.handleChange} />
-        <input type="submit" value="Send" />
+        <input type="text" value={this.state.roomId} onChange={this.handleChange}/>
+        <input type="text" value={this.state.content} onChange={this.handleChange}/>
+        <input type="text" value={this.state.username} onChange={this.handleChange}/>
+        <input type="text" value={this.state.sentAt} onChange={this.handleChange}/>
+        <input type="submit" value="reply" />
       </form>
+    );
+     const messageList = (
+    this.state.messages.map((message) => {
+      if (message.roomId === activeRoom){
+        return
+
+        <li key={message.key}>{message.content}</li>
+      }
     })
   );
-
-
-    return (
-      <div>
-        <div>{messageBar}</div>
-        <div>{messageList}</div>
-      </div>
-    );
-  }
+   return(
+    <div>
+      <div>{messageBar}</div>
+      <ul>{messageList}</ul>
+      <ul>{
+    </div>
+  );
 }
-
-
-
-
-
-export default MessageList
+}
+   export default MessageList;
