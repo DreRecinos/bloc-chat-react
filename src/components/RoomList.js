6c4950ca950569199f50917af952c45dc5bf0f9c
Dre Recinos <drerecinos.web@gmail.com>
2018-10-16 15:28:13 -0700

    help 2 10.16

--- src/components/RoomList.js
+++ src/components/RoomList.js
@@ -17,36 +17,35 @@ export class RoomList extends Component {
      this.setState({ title : e.target.value});
  }
 
-  createRoom(e) {
-    e.preventDefault();
-     let RoomName = this.state.newRoom;
-     this.roomsRef.push({ title: this.state.title });
+ createRoom(e) {
+   e.preventDefault();
+   this.roomsRef.push({ title: this.state.title });
    this.setState({ title: "" });
  }
 
-  selectRoom(room) {
-    this.props.activeRoom(room);
-  }
-
-  componentDidMount() {
-    const room = snapshot.val();
-      room.key = snapshot.key;
-      this.setState({ rooms: this.state.rooms.concat( room ) });
-    }
-  }
-
-   render() {
-     const roomForm = (
-      <form onSubmit={this.createRoom}>
-        <input type="text" value={this.state.title} placeholder="New Room Name" onChange={this.handleChange}/>
-        <input type="submit" value="New Chat" />
-      </form>
-   );
-
-     const roomList = this.state.rooms.map((room) =>
-       <li key={room.key}>{room.name}{room.title}</li>
-   );
+ componentDidMount() {
+   this.roomsRef.on('child_added', snapshot => {
+     const room = snapshot.val();
+     room.key = snapshot.key;
+     this.setState({ rooms: this.state.rooms.concat(room) })
+   });
  }
+
+ selectRoom(room) {
+   this.props.activeRoom(room);
+ }
+
+ render() {
+   const roomForm = (
+     <form onSubmit={this.createRoom}>
+       <input type="text" value={this.state.title} placeholder="Enter Room Name" onChange={this.handleChange}/>
+       <input type="submit" value="Create" />
+     </form>
+   );
+
+   const roomList = this.state.rooms.map((room) =>
+     <li key={room.key} onClick={(e) => this.selectRoom(room, e)}>{room.title}</li>
+   );
 
    return(
      <div>
@@ -56,5 +55,3 @@ export class RoomList extends Component {
    );
  }
 }
-
-export default RoomList;
